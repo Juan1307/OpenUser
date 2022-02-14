f3db8b9f9d20661649bf8632f1d3e1d58ba8217f
Antik <chuquirunajuan@gmail.com>
2022-02-13 20:53:22 -0500

    deploy ReactApp: v3

---
+++ docs/dist/NotFound.50d2e07b.js
@@ -0,0 +1,2 @@
+import{a as o,j as r}from"./index.7355d09c.js";function e({message:s,custom:a=""}){return o("div",{className:"dark fw-bolder "+a,children:[r("i",{className:"bx bx-block"})," ",s]})}export{e as N};
+//# sourceMappingURL=NotFound.50d2e07b.js.map
